c92625bdc96f04b32abc56e07973655f19905cd4
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-04 12:28:38 +1300

    feat: Added Connection To MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
-const http = require('http')
+// Dotenv configuration
+require('dotenv').config()
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'application/json'})
-  response.end(JSON.stringify(notes))
+// Import server components
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const registration = require('./models/registration')
+
+// Server setup
+const app = express()
+const PORT = process.env.PORT
+
+
+app.use(cors())
+app.use(express.json())
+
+// Application requests
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+// Application event listener
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+
