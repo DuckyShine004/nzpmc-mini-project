4823a08c99ff050aca8610735c003da3840e3b96
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-04 01:12:32 +1300

    feat: Added Simple Form Unfinished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,25 @@ import {useState} from 'react'
 import Heading from "./components/Heading"
 
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+  }
+
+  const onNoteChange = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div>
       <Heading heading={"Registration Page"} />
+      <form onSubmit={addNote}>
+        <input
+          value={"lol"}
+          onChange={onNoteChange}
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
   )
 }
