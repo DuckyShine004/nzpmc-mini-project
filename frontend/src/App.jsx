6e21bd280cc9513f850874de1cdc873e0c8454c3
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-04 01:35:46 +1300

    feat: Created Registration Form Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,18 @@
 import {useState} from 'react'
 
 import Heading from "./components/Heading"
+import RegistrationForm from './components/RegistrationForm'
 
 const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log(event.target)
-  }
-
-  const onNoteChange = (event) => {
-    console.log(event.target.value)
+  const addNote = ({name, dob}) => {
+    // Handle frontend backend connection
+    console.log(name, dob)
   }
 
   return (
     <div>
-      <Heading heading={"Registration Page"} />
-      <form onSubmit={addNote}>
-        <input
-          value={"lol"}
-          onChange={onNoteChange}
-        />
-        <button type="submit">submit</button>
-      </form>
+      <Heading heading="Registration Page" />
+      <RegistrationForm onFormSubmit={addNote} />
     </div>
   )
 }
