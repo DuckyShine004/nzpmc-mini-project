c9e55f8db9f7e86a9ab2b2eb2edccf4fd306618f
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-04 21:28:47 +1300

    feat: Added Button Which Redirect Users To Registration Page

--- frontend/src/components/ViewingPage.jsx
+++ frontend/src/components/ViewingPage.jsx
@@ -1,14 +1,18 @@
 import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 import registrationService from '../services/registration'
 
 import Heading from "./Heading"
 import UserList from './UserList'
+import Button from './Button'
 
 const baseURL = '/api/users'
 
 const ViewingPage = () => {
   const [users, setUsers] = useState([])
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -27,6 +31,7 @@ const ViewingPage = () => {
     <div>
       <Heading heading="Viewing Page" />
       <UserList users={users} />
+      <Button onClick={() => navigate('/')} text="Go To Registration Page" />
     </div>
   )
 }
