40304bcf841c293531b152ab858e79726e919884
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-04 22:32:30 +1300

    feat: Added Search Bar Option For User

--- frontend/src/components/viewing-page/ViewingPage.jsx
+++ frontend/src/components/viewing-page/ViewingPage.jsx
@@ -6,30 +6,48 @@ import registrationService from '../../services/registration'
 import Heading from '../utilities/Heading'
 import UserList from '../utilities/UserList'
 import Button from '../utilities/Button'
+import FormInput from '../utilities/FormInput'
 
 const baseURL = '/api/users'
 
 const ViewingPage = () => {
   const [users, setUsers] = useState([])
+  const [searchUser, setSearchUser] = useState('')
 
   const navigate = useNavigate()
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const userData = await registrationService.getAllUser(baseURL)
-        setUsers(userData)
-      } catch (error) {
-        console.error('Error fetching users:', error.message)
-      }
+  const fetchUsers = async () => {
+    try {
+      const userData = await registrationService.getAllUser(baseURL)
+      setUsers(userData)
+    } catch (error) {
+      console.error('Error fetching users:', error.message)
     }
+  }
 
+  useEffect(() => {
     fetchUsers()
   }, [])
+
+  const handleSearch = (event) => {
+    setSearchUser(event.target.value)
+  }
+
+  const handleSubmitSearch = () => {
+    fetchUsers()
+  }
 
   return (
     <div>
       <Heading heading="Viewing Page" />
+      <FormInput
+        label="text"
+        type="text"
+        value={searchUser}
+        onChange={handleSearch}
+        placeHolder="Search For User By Name"
+      />
+      <Button onClick={handleSubmitSearch} text="Search" />
       <UserList users={users} />
       <Button onClick={() => navigate('/')} text="Go To Registration Page" />
     </div>
