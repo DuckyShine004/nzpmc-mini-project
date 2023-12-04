753d67411506b8a46626730b02dc482e045f2ddc
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-04 15:15:15 +1300

    feat: Create Routing For Registration Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import {useState} from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Heading from "./components/Heading"
 import RegistrationForm from './components/RegistrationForm'
 
 const App = () => {
-  const addNote = ({name, dob}) => {
-    // Handle frontend backend connection
-    console.log(name, dob)
-  }
-
   return (
-    <div>
-      <Heading heading="Registration Page" />
-      <RegistrationForm onFormSubmit={addNote} />
-    </div>
+    <Router>
+      <div>
+        <Heading heading="Registration Page" />
+        <Routes>
+          <Route path="/" element={<RegistrationForm />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
